0331947a693588dc2f61dbbbd5ec649a5e610071
Panshoots <fr.juillet@profesor.duoc.cl>
2023-08-16 13:01:59 -0400

    ADD: se agregan paginas varias

--- src/app/pages/jugadores/jugadores.page.ts
+++ src/app/pages/jugadores/jugadores.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-jugadores',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JugadoresPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+
 
 }
